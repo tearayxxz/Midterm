36833431c7e63d538f25ed4e41d96811ade94c4d
SiRoRAY <dotv55@yahoo.com>
2021-10-02 23:18:00 +0700

    no message

---
+++ 1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+enum Menu //สร้างหน้าเมนูโดยการกำหนดค่า
+{
+    PlayGame = 1,//กำหนดค่า
+    Exit = 2//กำหนดค่า
+}
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menuscreen(); //เรียกMethod Menuscreen
+            InputMenu(); //เรียกMethod InputMenu
+            Game(); //เรียกMethod Game
+        }
+        static void Game() //สร้างmethod Game
+        {
+            HeadLine();//เรียกMethod HeadLine
+            Random random = new Random((int)DateTime.Now.Ticks); //ใช้คำสั่งrandom
+
+            string[] Space = { "Tennis", "Football", "Badminton" }; //ใส่คำที่จะrandom
+
+            string Guessing = Space[random.Next(0, Space.Length)]; //ใช้คำสั่งrandom
+            string ConvertToUppercase = Guessing.ToUpper();//แปลงเป็นตัวใหญ่เพื่อให้ง่ายจ่อการตรวจสอบ
+
+            StringBuilder ShowingDisplay = new StringBuilder(Guessing.Length); //ประกาศClass StringBuilder
+            for (int i = 0; i < Guessing.Length; i++) //สร้างลูปที่แสดงช่องว่างตามตัวอักษรจากข้อความ
+            {
+                ShowingDisplay.Append('_'); //แสดง"_"ตามตัวอักษรจากข้อความ
+            }
+
+            List<char> CheckCorrect = new List<char>(); //List ตัวอักษรที่ถูก
+            List<char> CheckIncorrect = new List<char>(); //List ตัวอักษรที่ผิด
+
+            int Incorrect = 0; //สร้างตัวแปรของจำนวนที่ผิดได้
+            bool win = false; //สร้างตัวแปรของการชนะ
+            int CharReavel = 0; //สร้างตัวแปรของตัวอักษรที่จะปรากฎ
+
+            string input; //สร้างตัวแปรของinput
+            char guess; //สร้างตัวแปรของตัวอักษรที่เเราจะทำการกรอกค่า
+
+            while (!win && Incorrect != 7) //สร้างลูปกำหนดเงื่อนไข
+            {
+                Console.Write("Input letter alphabet: ");//แสดงข้อความ
+
+                input = Console.ReadLine().ToUpper();//อ่านค่าที่เรากรอก
+                guess = input[0];//รับค่า
+
+                if (CheckCorrect.Contains(guess))//กำหนดเงื่อนไข ตรวจสอบคำที่ถูกและใช้ไปแล้ว
+                {
+                    Console.WriteLine("You've already tried '{0}', and it was correct!", guess);//แสดงข้อความโดยอ้างอิงจากguess
+                    continue;//ถ้าเงื่อนไขเป็นจริงก็ดำเนินการทำคำสั่งในลูปต่อไป
+                }
+                else if (CheckIncorrect.Contains(guess))//กำหนดเงื่อนไข ตรวจสอบคำที่ผิดและใช้ไปแล้ว
+                {
+                    Console.WriteLine("You've already tried '{0}', and it was wrong!", guess);//แสดงข้อความโดยอ้างอิงจากguess
+                    continue;//ถ้าเงื่อนไขเป็นจริงก็ดำเนินการทำคำสั่งในลูปต่อไป
+                }
+
+                if (ConvertToUppercase.Contains(guess))//กำหนดเงื่อนไข
+                {
+                    CheckCorrect.Add(guess);//เพิ่มคำที่เรากรอกไปลงในListที่ถูก
+                    Console.Clear();//เคลียร์หน้าจอ
+                    Console.WriteLine("Play Game Hangman"); //แสดงข้อความ
+                    Console.WriteLine("----------------------------------------");//แสดงข้อความ
+                    Console.WriteLine("Incorrect Score: {0}", Incorrect);//แสดงข้อความโดยอ้างอิงจากIncorrect
+                    for (int i = 0; i < Guessing.Length; i++) //สร้างลูป
+                    {
+                        if (ConvertToUppercase[i] == guess) //สร้างเงื่อนไขในขณะที่กำลังเล่น
+                        {
+                            ShowingDisplay[i] = Guessing[i]; //โชว์เมื่อกรอกถูก
+                            CharReavel++; //เพิ่มค่าCharReavel
+                        }
+                    }
+
+                    if (CharReavel == Guessing.Length) //กำหนดเงื่อนไขที่ทำให้ชนะ
+                    {
+                        win = true; //ชนะ
+                    }
+                }
+                else //นอกเหนือจากที่เรากำหนด
+                {
+                    CheckIncorrect.Add(guess);//เพิ่มคำที่เรากรอกไปลงในListที่ผิด
+                    Console.Clear();//เคลียร์หน้าจอ
+                    Console.WriteLine("Play Game Hangman");//แสดงข้อความ
+                    Console.WriteLine("----------------------------------------");//แสดงข้อความ
+                    Console.WriteLine("Incorrect Score: {0}", Incorrect+1);//แสดงข้อความโดยอิงจาก Incorrect
+                    Console.WriteLine("Nope, there's no '{0}' in it!", guess);//แสดงข้อความโดยอิงจาก guess
+                    Incorrect++;//เพิ่มค่าที่กรอก
+                }
+                Console.WriteLine(ShowingDisplay.ToString());//แสดงข้อความของShowingDisplay.ToString()
+            }
+            result(win, Guessing); //เรียกmethod result เพื่อแสดงผลลัพธ์
+        }
+        static void Menuscreen() //สร้างMethod Menuscreen
+        {
+            Console.WriteLine("Welcome to Hangman Game");//แสดงข้อความ
+            Console.WriteLine("---------------------------------");//แสดงข้อความ
+            Console.WriteLine("1.Play game");//แสดงข้อความ
+            Console.WriteLine("2.Exit");//แสดงข้อความ
+        }
+        static void InputMenu() //สร้างMethod InputMenu
+        {
+            Console.Write("Please Select Menu: ");//แสดงข้อความ
+            Menu menu = (Menu)(int.Parse(Console.ReadLine()));//อ่านค่าที่เรากรอก
+            Console.Clear();//เคลียร์หน้าจอ
+            PresentMenu(menu);//เรียกmethod PresentMenu
+        }
+        static void PresentMenu(Menu menu) //สร้างMethod PresentMenu
+        {
+            if (menu == Menu.PlayGame) //กำหนดเงื่อนไข
+            {
+                Game(); //เรียกmethodGame
+            }
+            else if(menu == Menu.Exit) //กำหนดเงื่อนไข
+            {
+                Exit(); //เรียกmethodExit
+            }
+        }
+        public static void HeadLine() //สร้างMethod HeadLine
+        {
+            Console.WriteLine("Play Game Hangman");//แสดงข้อความ
+            Console.WriteLine("----------------------------------------");//แสดงข้อความ
+            Console.WriteLine("Incorrect Score: 0");//แสดงข้อความ
+        }
+        static void result(bool won, string wordToGuess) //สร้างMethod result
+        {
+            if (won)//กำหนดเงื่อนไข
+            {
+                Console.WriteLine("\n");//เว้นบรรทัด
+                Console.WriteLine("----------------------------------------");//แสดงข้อความ
+                Console.WriteLine("                You win!!               ");//แสดงข้อความ
+                Console.WriteLine("----------------------------------------");//แสดงข้อความ
+                Console.WriteLine("\n");//เว้นบรรทัด
+            }
+            else //นอกเหนือเงื่อนไข
+            {
+                Console.WriteLine("\n");//เว้นบรรทัด
+                Console.WriteLine("----------------------------------------");//แสดงข้อความ
+                Console.WriteLine("                Game Over               ");//แสดงข้อความ
+                Console.WriteLine("----------------------------------------");//แสดงข้อความ
+                Console.WriteLine("\n");//เว้นบรรทัด
+            }
+
+            Console.Write("Press ENTER to exit...");//แสดงข้อความ
+            Environment.Exit(0); //จบการทำงาน
+        }
+        static void Exit()//สร้างMethod Exit
+        {
+            Environment.Exit(0); //จบการทำงาน
+        }
+    }
+}
---
+++ 2.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+enum Menu
+{
+    Login = 1,
+    Register = 2
+}
+namespace _2
+{
+    class BookStore
+    {
+        public List<string> BorrowList = new List<string>();
+        List<string> Borrowing = new List<string>();
+        public BookStore()
+        {
+            BorrowList.Add("NOW I UNDERSTAND");
+            BorrowList.Add("REVOLUTIONARY WEALTH");
+            BorrowList.Add("Six Degrees");
+            BorrowList.Add("Les Vacances");
+        }
+        public void addToCart(string name)
+        {
+            Borrowing.Add(name);
+        }
+
+        public void BorrowResult()
+        {
+            if (Borrowing.Count == 0)
+            {
+                Console.WriteLine("No borrowing");
+            }
+            else
+            {
+                Console.WriteLine("Brrowing List:");
+                foreach (string i in Borrowing)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+    class Data
+    {
+        public string User;
+        public string PassWord;
+        public string Type;
+    }
+    class Registor : Data
+    {
+        private List<Data> Account;
+        public Registor()
+        {
+            Account = new List<Data>();
+        }
+        public void addList(Data account)
+        {
+            Account.Add(account);
+        }
+        public void getList()
+        {
+            foreach (Data account in Account)
+            {
+                if(account.Type == "1")
+                {
+                    Console.WriteLine("Student Management");
+                    Console.WriteLine("------------------");
+                    Console.WriteLine("Name:{0}", account.User);
+                    Console.WriteLine("Student ID:62130701103", account.Type);
+                }
+                else if (account.Type == "2")
+                {
+                    Console.WriteLine("Employee Management");
+                    Console.WriteLine("------------------");
+                    Console.WriteLine("Name:{0}", account.User);
+                    Console.WriteLine("Employee ID:5214785", account.Type);
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string decide = "y";
+            string selectBook = "";
+            BookStore bookStore = new BookStore();
+            Registor ListUser = new Registor();
+            Menuscreen();
+            InputMenu(ListUser, selectBook, decide, bookStore);
+        }
+        static void Menuscreen()
+        {
+            Console.WriteLine("Welcome to Digital Library");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("1.Login");
+            Console.WriteLine("2.Register");
+        }
+        static void InputMenu(Registor ListUser, string selectBook, string decide, BookStore bookStore)
+        {
+            Console.Write("Select Menu: ");
+            Menu menu = (Menu)int.Parse(Console.ReadLine());
+            PresentMenu(menu, ListUser, selectBook, decide, bookStore);
+        }
+        static void PresentMenu(Menu menu, Registor ListUser, string selectBook, string decide, BookStore bookStore)
+        {
+            if (menu == Menu.Login)
+            {
+                Loginn(ListUser, selectBook,  decide,  bookStore);
+            }
+            else if (menu == Menu.Register)
+            {
+                Registoration(ListUser, selectBook, decide, bookStore);
+            }
+        }
+        static void Registoration(Registor ListUser, string selectBook, string decide, BookStore bookStore)
+        {
+            Console.Clear();
+            Data B = new Data();
+
+            Console.WriteLine("Register new Person");
+            Console.WriteLine("------------------");
+
+            Console.Write("Input name: ");
+            B.User = Console.ReadLine();
+
+            Console.Write("Input Password: ");
+            B.PassWord = Console.ReadLine();
+
+            Console.Write("Input User Type 1 = Student, 2 = Employee: ");
+            B.Type = Console.ReadLine();
+
+            if (B.Type == "1")
+            {
+                Console.WriteLine("Student ID: 62130701103");
+            }
+            else if (B.Type == "2")
+            {
+                Console.WriteLine("Employee ID: 5214785");
+            }
+            ListUser.addList(B);
+            Console.WriteLine("Done");
+            
+            Console.Clear();
+            Menuscreen();
+            InputMenu(ListUser, selectBook, decide, bookStore);
+        }
+        static void Loginn(Registor ListUser, string selectBook, string decide, BookStore bookStore)
+        {
+            Console.Clear();
+            Console.WriteLine("Login Screen");
+            Console.WriteLine("------------------");
+                Console.Write("Input name: ");
+            string LoginUser = Console.ReadLine();
+            Console.Write("Input Password: ");
+            string LoginPass = Console.ReadLine();
+            if (LoginUser == "kittinum")
+            {
+                if (LoginPass == "1234")
+                {
+                    ListUser.getList();
+                    Console.WriteLine("------------------");
+                    Book(selectBook, decide, bookStore);
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("------------------");
+                Console.WriteLine("  Worng!!!");
+                Console.WriteLine("------------------");
+                Console.WriteLine("  Hint\n" +
+                    "Name: kittinum\n" +
+                    "Password: 1234");
+                Console.WriteLine("------------------");
+                Console.WriteLine("Please Enter to continue");
+                Console.ReadLine();
+                Console.Clear();
+                Menuscreen();
+                InputMenu( ListUser, selectBook, decide, bookStore);
+            }
+        }
+        static void Book(string selectBook, string decide, BookStore bookStore)
+        {
+            do
+            {
+                Console.WriteLine("Book List");
+                Console.WriteLine("------------");
+                foreach (string i in bookStore.BorrowList)
+                {
+                    Console.WriteLine("Book ID: {0}", (bookStore.BorrowList.IndexOf(i) + 1) + " ");
+                    Console.Write("Book name: ");
+                    Console.WriteLine(i);
+                }
+                Console.Write("Input book ID : ");
+                selectBook = Console.ReadLine();
+                switch (selectBook)
+                {
+                    case "1":
+                        bookStore.addToCart(bookStore.BorrowList[0]);
+                        Console.WriteLine("Added " + bookStore.BorrowList[0]);
+                        break;
+                    case "2":
+                        bookStore.addToCart(bookStore.BorrowList[1]);
+                        Console.WriteLine("Added " + bookStore.BorrowList[1]);
+                        break;
+                    case "3":
+                        bookStore.addToCart(bookStore.BorrowList[2]);
+                        Console.WriteLine("Added " + bookStore.BorrowList[2]);
+                        break;
+                    case "4":
+                        bookStore.addToCart(bookStore.BorrowList[3]);
+                        Console.WriteLine("Added " + bookStore.BorrowList[3]);
+                        break;
+                    default:
+                        Console.WriteLine("Not Added to cart. found select number of flower");
+                        break;
+                }
+                Console.WriteLine("You can stop this progress ? exit for >> exit << progress and pressany key for continue");
+                decide = Console.ReadLine();
+                Console.Clear();
+            }
+            while (decide != "exit");
+            bookStore.BorrowResult();
+        }
+    }
+}
